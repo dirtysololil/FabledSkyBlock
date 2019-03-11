2ae640db983c923006a7b91d98999f24525cd91d
Esophose <esophose@gmail.com>
2019-03-11 04:23:05 -0600

    Fix compiling issues

--- src/main/kotlin/com/songoda/fabledskyblock/FabledSkyBlock.kt
+++ src/main/kotlin/com/songoda/fabledskyblock/FabledSkyBlock.kt
@@ -1,24 +1,40 @@
 package com.songoda.fabledskyblock
 
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
-
+import org.bukkit.command.ConsoleCommandSender
+import org.bukkit.event.HandlerList
 
 class FabledSkyBlock : JavaPlugin() {
 
     companion object {
         var instance: FabledSkyBlock? = null
-        private set
+            private set
     }
 
     override fun onEnable() {
-        instance = this
+        val console = Bukkit.getConsoleSender()
+        console.sendMessage(formatText("&a============================="))
+        console.sendMessage(formatText("&7${this.description.name} " + this.description.version + " by &5${this.description.authors[0]} <3&7!"))
+        console.sendMessage(formatText("&7Action: &aEnabling&7..."))
+        console.sendMessage(formatText("&a============================="))
 
-        Bukkit.broadcastMessage("Enabled test")
+        instance = this
     }
 
     override fun onDisable() {
-        Bukkit.broadcastMessage("Disabled test")
+        val console = Bukkit.getConsoleSender()
+        console.sendMessage(formatText("&a============================="))
+        console.sendMessage(formatText("&7${this.description.name} ${this.description.version} by &5${this.description.authors[0]} <3&7!"))
+        console.sendMessage(formatText("&7Action: &cDisabling&7..."))
+        console.sendMessage(formatText("&a============================="))
+
+        HandlerList.unregisterAll(this)
+    }
+
+    private fun formatText(input: String): String {
+        return ChatColor.translateAlternateColorCodes('&', input)
     }
 
 }
